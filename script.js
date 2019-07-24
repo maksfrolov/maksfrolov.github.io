3d2ef7e39cf9662c627f13eb55179de59f1f71c7
maksfrolov <be.your.side@yandex.ru>
2019-07-24 13:17:29 +0400

    dada

--- script.js
+++
@@ -1,1 +0,0 @@
-var o,l=prompt("деление на 2",0),e=l/2;console.log("one"),console.log("string"),console.log(o),console.log(typeof o),console.log(0),console.log("number"),alert(l),console.log(typeof!0),alert(e),console.log(typeof e);
